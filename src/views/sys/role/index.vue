300a2dc0efc57a1aaa28aa5ccde60b97a13bb5d4
shi_yi <shi_yidashi@163.com>
2020-09-08 17:54:52 +0800

    角色管理 开始

--- src/views/sys/role/index.vue
+++ src/views/sys/role/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <div style="margin: 10px 0;">
+      <el-button-group>
+        <el-button type="primary" @click="create" icon="el-icon-edit">创建角色</el-button>
+      </el-button-group>
+    </div>
+    <list ref="list"></list>
+  </div>
 </template>
 
 <script>
+import list from '@/components/sys/role/list'
+
 export default {
-  name: ''
+  name: 'Role',
+  components: {
+    list
+  },
+  methods: {
+    create () {
+
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
 </style>
